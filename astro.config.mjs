cf8a9129b81d5f3d5ed8c859e5a2cda964b9cd57
Benjamin Menant <dev@menant-benjamin.fr>
2023-06-26 11:02:12 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:3000',
-  //output: 'server', // works
-  output: 'hybrid', // img not rendered on ssr pages
+  output: 'hybrid', // or 'server' (same issue)
   adapter: node({ mode: 'middleware' }),
   experimental: { assets: true },
 });
